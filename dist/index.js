4a45e80eb3cd8db42e496b4a7bbb8e4ac48b2137
k3nsei <k3nsei.pl@gmail.com>
2017-03-30 20:28:29 +0200

    Refactoring

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,6 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./in-viewport.module", "./in-viewport.service", "./in-viewport-config.class", "./in-viewport.directive"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var in_viewport_module_1 = require("./in-viewport.module");
-    exports.InViewportModule = in_viewport_module_1.InViewportModule;
-    var in_viewport_service_1 = require("./in-viewport.service");
-    exports.InViewportService = in_viewport_service_1.InViewportService;
-    var in_viewport_config_class_1 = require("./in-viewport-config.class");
-    exports.InViewportConfig = in_viewport_config_class_1.InViewportConfig;
-    exports.InViewportConfigDirection = in_viewport_config_class_1.InViewportConfigDirection;
-    var in_viewport_directive_1 = require("./in-viewport.directive");
-    exports.InViewportDirective = in_viewport_directive_1.InViewportDirective;
-});
+import { InViewportModule } from "./in-viewport.module";
+import { InViewportService } from "./in-viewport.service";
+import { InViewportConfig, InViewportConfigDirection } from './in-viewport-config.class';
+import { InViewportDirective } from "./in-viewport.directive";
+export { InViewportModule, InViewportService, InViewportConfig, InViewportConfigDirection, InViewportDirective };
 //# sourceMappingURL=index.js.map
--- src/in-viewport.service.ts
+++ src/in-viewport.service.ts
@@ -1,7 +1,7 @@
 import { EventEmitter, Injectable } from "@angular/core";
 
 export interface InViewportServiceRegistryObject {
-  target: Element,
+  targets: Element[],
   rootElement: Element,
   observer: IntersectionObserver
 }
@@ -9,7 +9,7 @@ export interface InViewportServiceRegistryObject {
 @Injectable()
 export class InViewportService {
   protected registry: InViewportServiceRegistryObject[];
-  public trigger$: EventEmitter<IntersectionObserverEntry>;
+  public trigger$: EventEmitter<IntersectionObserverEntry[]>;
 
   constructor() {
     this.registry = [];
@@ -17,47 +17,51 @@ export class InViewportService {
   }
 
   protected onChanges(entries: IntersectionObserverEntry[]): void {
-    this.trigger$.emit(entries[0]);
+    this.trigger$.emit(entries);
   }
 
-  protected findTarget(target: Element) {
-    return this.registry.find((item) => item.target === target);
+  protected findRegistryEntry(rootElement: Element) {
+    return this.registry.find((item) => item.rootElement === rootElement);
   }
 
   public addTarget(target: Element, rootElement?: Element): void {
-    if (!this.findTarget(target)) {
-      // Create target observer options
-      const observerOptions: any = {
+    let registryEntry = this.findRegistryEntry(rootElement);
+    if (!registryEntry) {
+      const registryEntryObserverOptions: any = {
         threshold: Array(101).fill(void 0).map((item, i) => (i / 100))
       };
-
       if (rootElement) {
-        observerOptions.root = rootElement;
+        registryEntryObserverOptions.root = rootElement;
       }
-
-      // Create target object
-      const targetObj = {
-        target,
+      registryEntry = {
+        targets: [target],
         rootElement,
         observer: new IntersectionObserver(
           (entries: IntersectionObserverEntry[]) => this.onChanges(entries),
-          observerOptions
+          registryEntryObserverOptions
         )
       };
-
-      // Start to observe target
-      targetObj.observer.observe(target);
-
-      // Add target to registry
-      this.registry.push(targetObj);
+      registryEntry.observer.observe(target);
+      this.registry.push(registryEntry);
+    } else if (registryEntry.targets.indexOf(target) < 0) {
+      registryEntry.targets.push(target);
+      registryEntry.observer.observe(target);
     }
   }
 
-  public removeTarget(target: Element): void {
-    const targetObj = this.findTarget(target);
-    if (targetObj) {
-      targetObj.observer.disconnect();
-      this.registry.splice(this.registry.indexOf(targetObj), 1);
+  public removeTarget(target: Element, rootElement?: Element): void {
+    const registryEntry = this.findRegistryEntry(rootElement);
+    const registryEntryIdx = this.registry.indexOf(registryEntry);
+    if (registryEntry) {
+      const targetIdx = registryEntry.targets.indexOf(target);
+      if (targetIdx >= 0) {
+        registryEntry.observer.unobserve(target);
+        registryEntry.targets.splice(targetIdx, 1);
+      }
+      if (registryEntry.targets.length === 0) {
+        registryEntry.observer.disconnect();
+        this.registry.splice(registryEntryIdx, 1);
+      }
     }
   }
 }
