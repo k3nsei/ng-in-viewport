1347bf50812dd3c407f31df83585e14a1f34f19a
k3nsei <k3nsei.pl@gmail.com>
2017-03-30 20:01:27 +0200

    Refactoring

--- dist/in-viewport.directive.js
+++ dist/in-viewport.directive.js
@@ -7,61 +7,79 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-import { Directive, ElementRef, EventEmitter, Input, Output } from "@angular/core";
-import { InViewportConfig } from "./in-viewport-config.class";
-import { InViewportService } from "./in-viewport.service";
-let InViewportDirective = class InViewportDirective {
-    constructor(elementRef, inViewportService) {
-        this.elementRef = elementRef;
-        this.inViewportService = inViewportService;
-        this.config = new InViewportConfig();
-        this.action$ = new EventEmitter();
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
     }
-    set updateConfig(value) {
-        if (value && Object.prototype.toString.call(value) === '[object Object]') {
-            if (value.rootElement instanceof Element) {
-                this.config.rootElement = value.rootElement;
-            }
-            if ('partial' in value) {
-                this.config.partial = value.partial;
-            }
-            if ('direction' in value) {
-                this.config.rootElement = value.direction;
-            }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "@angular/core", "./in-viewport-config.class", "./in-viewport.service"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var core_1 = require("@angular/core");
+    var in_viewport_config_class_1 = require("./in-viewport-config.class");
+    var in_viewport_service_1 = require("./in-viewport.service");
+    var InViewportDirective = (function () {
+        function InViewportDirective(elementRef, inViewportService) {
+            this.elementRef = elementRef;
+            this.inViewportService = inViewportService;
+            this.config = new in_viewport_config_class_1.InViewportConfig();
+            this.action$ = new core_1.EventEmitter();
         }
-    }
-    ngAfterViewInit() {
-        this.inViewportService.trigger$.subscribe((entry) => this.check(entry));
-        this.inViewportService.addTarget(this.elementRef.nativeElement, this.config.rootElement);
-    }
-    ngOnDestroy() {
-        this.inViewportService.removeTarget(this.elementRef.nativeElement);
-    }
-    check(entry) {
-        if (entry.target === this.elementRef.nativeElement) {
-            const value = this.config.partial ? (entry.intersectionRatio > 0) : (entry.intersectionRatio === 1);
-            this.action$.emit({
-                target: entry.target,
-                value
-            });
-        }
-    }
-};
-__decorate([
-    Output('inViewportAction'),
-    __metadata("design:type", EventEmitter)
-], InViewportDirective.prototype, "action$", void 0);
-__decorate([
-    Input('inViewportOptions'),
-    __metadata("design:type", Object),
-    __metadata("design:paramtypes", [Object])
-], InViewportDirective.prototype, "updateConfig", null);
-InViewportDirective = __decorate([
-    Directive({
-        selector: '[in-viewport], [inViewport]'
-    }),
-    __metadata("design:paramtypes", [ElementRef,
-        InViewportService])
-], InViewportDirective);
-export { InViewportDirective };
+        Object.defineProperty(InViewportDirective.prototype, "updateConfig", {
+            set: function (value) {
+                if (value && Object.prototype.toString.call(value) === '[object Object]') {
+                    if (value.rootElement instanceof Element) {
+                        this.config.rootElement = value.rootElement;
+                    }
+                    if ('partial' in value) {
+                        this.config.partial = value.partial;
+                    }
+                    if ('direction' in value) {
+                        this.config.rootElement = value.direction;
+                    }
+                }
+            },
+            enumerable: true,
+            configurable: true
+        });
+        InViewportDirective.prototype.ngAfterViewInit = function () {
+            var _this = this;
+            this.inViewportService.trigger$.subscribe(function (entry) { return _this.check(entry); });
+            this.inViewportService.addTarget(this.elementRef.nativeElement, this.config.rootElement);
+        };
+        InViewportDirective.prototype.ngOnDestroy = function () {
+            this.inViewportService.removeTarget(this.elementRef.nativeElement);
+        };
+        InViewportDirective.prototype.check = function (entry) {
+            if (entry.target === this.elementRef.nativeElement) {
+                var value = this.config.partial ? (entry.intersectionRatio > 0) : (entry.intersectionRatio === 1);
+                this.action$.emit({
+                    target: entry.target,
+                    value: value
+                });
+            }
+        };
+        return InViewportDirective;
+    }());
+    __decorate([
+        core_1.Output('inViewportAction'),
+        __metadata("design:type", core_1.EventEmitter)
+    ], InViewportDirective.prototype, "action$", void 0);
+    __decorate([
+        core_1.Input('inViewportOptions'),
+        __metadata("design:type", Object),
+        __metadata("design:paramtypes", [Object])
+    ], InViewportDirective.prototype, "updateConfig", null);
+    InViewportDirective = __decorate([
+        core_1.Directive({
+            selector: '[in-viewport], [inViewport]'
+        }),
+        __metadata("design:paramtypes", [core_1.ElementRef,
+            in_viewport_service_1.InViewportService])
+    ], InViewportDirective);
+    exports.InViewportDirective = InViewportDirective;
+});
 //# sourceMappingURL=in-viewport.directive.js.map
