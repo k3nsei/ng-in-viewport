c72d0068e80192d17bc4db7d80c22b9fbbdfd10e
Piotr Stępniewski <190422+k3nsei@users.noreply.github.com>
2017-03-30 21:47:18 +0200

    Refactoring

--- dist/in-viewport.module.d.ts
+++ dist/in-viewport.module.d.ts
@@ -1,4 +1,7 @@
 import { ModuleWithProviders } from "@angular/core";
+import { InViewportService } from "./in-viewport.service";
 export declare class InViewportModule {
+    private inViewportService;
     static forRoot(): ModuleWithProviders;
+    constructor(inViewportService: InViewportService);
 }
--- dist/in-viewport.module.js
+++ dist/in-viewport.module.js
@@ -4,11 +4,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 import { NgModule } from "@angular/core";
 import { InViewportDirective } from "./in-viewport.directive";
 import { InViewportService } from "./in-viewport.service";
 var InViewportModule = InViewportModule_1 = (function () {
-    function InViewportModule() {
+    function InViewportModule(inViewportService) {
+        this.inViewportService = inViewportService;
     }
     InViewportModule.forRoot = function () {
         return {
@@ -29,7 +33,8 @@ InViewportModule = InViewportModule_1 = __decorate([
         exports: [
             InViewportDirective
         ]
-    })
+    }),
+    __metadata("design:paramtypes", [InViewportService])
 ], InViewportModule);
 export { InViewportModule };
 var InViewportModule_1;
