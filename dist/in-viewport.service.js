a0b57709bd7ce482123ec310772f219a02706600
Piotr Stępniewski <190422+k3nsei@users.noreply.github.com>
2017-03-30 20:01:27 +0200

    Refactoring

--- dist/in-viewport.service.js
+++ dist/in-viewport.service.js
@@ -7,50 +7,64 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-import { EventEmitter, Injectable } from "@angular/core";
-let InViewportService = class InViewportService {
-    constructor() {
-        this.registry = [];
-        this.trigger$ = new EventEmitter();
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
     }
-    onChanges(entries) {
-        this.trigger$.emit(entries[0]);
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "@angular/core"], factory);
     }
-    findTarget(target) {
-        return this.registry.find((item) => item.target === target);
-    }
-    addTarget(target, rootElement) {
-        if (!this.findTarget(target)) {
-            // Create target observer options
-            const observerOptions = {
-                threshold: Array(101).fill(void 0).map((item, i) => (i / 100))
-            };
-            if (rootElement) {
-                observerOptions.root = rootElement;
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var core_1 = require("@angular/core");
+    var InViewportService = (function () {
+        function InViewportService() {
+            this.registry = [];
+            this.trigger$ = new core_1.EventEmitter();
+        }
+        InViewportService.prototype.onChanges = function (entries) {
+            this.trigger$.emit(entries[0]);
+        };
+        InViewportService.prototype.findTarget = function (target) {
+            return this.registry.find(function (item) { return item.target === target; });
+        };
+        InViewportService.prototype.addTarget = function (target, rootElement) {
+            var _this = this;
+            if (!this.findTarget(target)) {
+                // Create target observer options
+                var observerOptions = {
+                    threshold: Array(101).fill(void 0).map(function (item, i) { return (i / 100); })
+                };
+                if (rootElement) {
+                    observerOptions.root = rootElement;
+                }
+                // Create target object
+                var targetObj = {
+                    target: target,
+                    rootElement: rootElement,
+                    observer: new IntersectionObserver(function (entries) { return _this.onChanges(entries); }, observerOptions)
+                };
+                // Start to observe target
+                targetObj.observer.observe(target);
+                // Add target to registry
+                this.registry.push(targetObj);
             }
-            // Create target object
-            const targetObj = {
-                target,
-                rootElement,
-                observer: new IntersectionObserver((entries) => this.onChanges(entries), observerOptions)
-            };
-            // Start to observe target
-            targetObj.observer.observe(target);
-            // Add target to registry
-            this.registry.push(targetObj);
-        }
-    }
-    removeTarget(target) {
-        const targetObj = this.findTarget(target);
-        if (targetObj) {
-            targetObj.observer.disconnect();
-            this.registry.splice(this.registry.indexOf(targetObj), 1);
-        }
-    }
-};
-InViewportService = __decorate([
-    Injectable(),
-    __metadata("design:paramtypes", [])
-], InViewportService);
-export { InViewportService };
+        };
+        InViewportService.prototype.removeTarget = function (target) {
+            var targetObj = this.findTarget(target);
+            if (targetObj) {
+                targetObj.observer.disconnect();
+                this.registry.splice(this.registry.indexOf(targetObj), 1);
+            }
+        };
+        return InViewportService;
+    }());
+    InViewportService = __decorate([
+        core_1.Injectable(),
+        __metadata("design:paramtypes", [])
+    ], InViewportService);
+    exports.InViewportService = InViewportService;
+});
 //# sourceMappingURL=in-viewport.service.js.map
