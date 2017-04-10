aa86985ea669e6561480064c441825c58e39fb67
k3nsei <k3nsei.pl@gmail.com>
2017-04-10 10:33:17 +0200

    preparing to 1.2.0

--- src/in-viewport/in-viewport.ts
+++ src/in-viewport/in-viewport.ts
@@ -2,5 +2,11 @@ import { InViewportModule } from "./in-viewport.module";
 import { InViewportService } from "./in-viewport.service";
 import { InViewportConfig, InViewportConfigDirection } from './in-viewport-config.class';
 import { InViewportDirective } from "./in-viewport.directive";
-export { InViewportModule, InViewportService, InViewportConfig, InViewportConfigDirection, InViewportDirective };
-//# sourceMappingURL=index.js.map
+
+export {
+  InViewportModule,
+  InViewportService,
+  InViewportConfig,
+  InViewportConfigDirection,
+  InViewportDirective
+};
