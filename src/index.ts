0ca550c5a370b5a8400ade14a0d0bf48fad702d6
Piotr Stępniewski <190422+k3nsei@users.noreply.github.com>
2017-04-10 10:33:17 +0200

    preparing to 1.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
-import { InViewportModule } from "./in-viewport.module";
-import { InViewportService } from "./in-viewport.service";
-import { InViewportConfig, InViewportConfigDirection } from './in-viewport-config.class';
-import { InViewportDirective } from "./in-viewport.directive";
+// This file is not used to build this module. It is only used during editing
+// by the TypeScript language service and during build for verification. `ngc`
+// replaces this file with production index.ts when it rewrites private symbol
+// names.
 
-export {
-  InViewportModule,
-  InViewportService,
-  InViewportConfig,
-  InViewportConfigDirection,
-  InViewportDirective
-};
+export * from './public-api';
