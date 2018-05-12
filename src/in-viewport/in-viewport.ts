91dc2d6628e5ed0a467d815da3b408990456a4d5
Piotr Stępniewski <k3nsei.pl@gmail.com>
2018-05-12 18:25:44 +0200

    Add Circle Ci Add Badges Fixes for: - issues/29 - issues/30 - issues/32

--- src/in-viewport/in-viewport.ts
+++ src/in-viewport/in-viewport.ts
@@ -1,6 +1,6 @@
 import { InViewportModule } from './in-viewport.module';
 import { InViewportService } from './in-viewport.service';
 import { InViewportConfig, InViewportConfigDirection } from './in-viewport-config.class';
-import { InViewportDirective } from './in-viewport.directive';
+import { InViewportDirective, InViewportMetadata } from './in-viewport.directive';
 
 export { InViewportModule, InViewportService, InViewportConfig, InViewportConfigDirection, InViewportDirective };
