79cdfeec7006d33e021cf6a3249cdb276fdac805
Piotr Stępniewski <190422+k3nsei@users.noreply.github.com>
2018-05-25 18:50:51 +0200

    ISSUES-12: - Add platform-server support

--- build.sh
+++ build.sh
@@ -1,40 +1,41 @@
 #!/usr/bin/env bash
 
-# Clean up previous distributions
-rm -rf dist
-rm -rf build
-
 # Variables
-NGC="node_modules/.bin/ngc"
-ROLLUP="node_modules/.bin/rollup"
+NGC="$PWD/node_modules/.bin/ngc"
+ROLLUP="$PWD/node_modules/.bin/rollup"
+BUILD_DIR="$PWD/build"
+DIST_DIR="$PWD/dist"
+DOCS_DIR="$PWD/docs"
 
-# Build ES2015 UMD Module
-$NGC -p src/tsconfig-build.json
-cp -R ./build ./dist/
-find ./dist -type f -name "*.js" -delete
-$ROLLUP build/index.js -o dist/index.umd.js -f umd --name="ng-in-viewport" -g "@angular/core:ng.core,rxjs/BehaviorSubject:BehaviorSubject"
+# Cleanup previous distributions
+rm -rf $DIST_DIR
+rm -rf $BUILD_DIR
 
 # Build ES2015 Module
-$NGC -p src/tsconfig-build.json
-cp -R ./build ./dist/
-find ./dist -type f -name "*.js" ! -name "index.umd.js" -delete
-$ROLLUP build/index.js -o dist/index.js -f es
-
-# Build ES5 UMD Module
-rm -rf build
-$NGC -p src/tsconfig-build-es5.json
-$ROLLUP build/index.js -o dist/index.es5.umd.js -f umd --name="ng-in-viewport" -g "@angular/core:ng.core,rxjs/BehaviorSubject:BehaviorSubject"
+$NGC -p $PWD/src/tsconfig-build.json
+cp -R $BUILD_DIR $DIST_DIR/
+find $DIST_DIR -type f -name "*.js" -delete
+$ROLLUP $BUILD_DIR/index.js -o $DIST_DIR/index.umd.js -f umd --name="ng-in-viewport" -g "@angular/core:ng.core,@angular/common:ng.common,rxjs/Subscription:Subscription,rxjs/Subject:Subject,rxjs/BehaviorSubject:BehaviorSubject"
+$ROLLUP $BUILD_DIR/index.js -o $DIST_DIR/index.js -f es
 
 # Build ES5 Module
-rm -rf build
-$NGC -p src/tsconfig-build-es5.json
-$ROLLUP build/index.js -o dist/index.es5.js -f es
+rm -rf $BUILD_DIR
+$NGC -p $PWD/src/tsconfig-build-es5.json
+$ROLLUP $BUILD_DIR/index.js -o $DIST_DIR/index.es5.umd.js -f umd --name="ng-in-viewport" -g "@angular/core:ng.core,@angular/common:ng.common,rxjs/Subscription:Subscription,rxjs/Subject:Subject,rxjs/BehaviorSubject:BehaviorSubject"
+$ROLLUP $BUILD_DIR/index.js -o $DIST_DIR/index.es5.js -f es
+
+# Generate Documentation
+bash $PWD/gen-docs.sh
+cp -R $DOCS_DIR $DIST_DIR/docs/
 
 # Copy package.json
-cp src/package.json dist/package.json
+cp $PWD/src/package.json $DIST_DIR/package.json
 
 # Copy README.md
-cp README.md dist/README.md
+cp $PWD/README.md $DIST_DIR/README.md
 
 # Copy LICENSE
-cp LICENSE dist/LICENSE
+cp $PWD/LICENSE $DIST_DIR/LICENSE
+
+# Cleanup after build
+rm -rf $BUILD_DIR
