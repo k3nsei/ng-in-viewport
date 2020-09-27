87bb5f7d194dcdf24f0f121d24f330285d1d747d
Eugene Kubesh <codbasters@gmail.com>
2020-09-27 20:04:07 +0300

    fix: Lint issues

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -25,7 +25,7 @@ import 'classlist.js'; // Run `npm install --save classlist.js`.
 /** IE10 and IE11 requires the following for the Reflect API. */
 import 'core-js/es6/reflect';
 
-/** Evergreen browsers require these. **/
+/** Evergreen browsers require these. */
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
 
@@ -33,7 +33,7 @@ import 'core-js/es7/reflect';
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- **/
+ */
 import 'web-animations-js'; // Run `npm install --save web-animations-js`.
 
 /**
@@ -41,6 +41,7 @@ import 'web-animations-js'; // Run `npm install --save web-animations-js`.
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  */
 
+// tslint:disable-next-line:no-commented-code
 // (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
 // (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
 // (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
