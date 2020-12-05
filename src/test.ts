cbf7033d8e6f5814180f923a04d5c8396c3ffd18
Piotr Stępniewski <k3nsei.pl@gmail.com>
2020-12-05 15:22:46 +0100

    chore: Migrate to Angular 11.0.3

--- src/test.ts
+++ src/test.ts
@@ -13,13 +13,14 @@
 
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 
 declare const require: {
   context(
     path: string,
     deep?: boolean,
-    filter?: RegExp,
+    filter?: RegExp
   ): {
     keys(): string[];
     <T>(id: string): T;
